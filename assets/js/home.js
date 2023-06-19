8331190c3ef25345eeb9673e04d0da5887a762ea
vitorinojoseneto <67804356+vitorinojoseneto@users.noreply.github.com>
2023-06-19 18:14:09 +0100

    up into github

---
+++ assets/js/home.js
@@ -0,0 +1,86 @@
+
+ function toggle() {
+     var x = document.getElementById("play_circle");
+     if(x.style.display == "none") {
+                x.style.display = "block";
+            }
+         else {
+                x.style.display = "none";
+     }
+     }
+
+  function shop() {
+        var x = document.getElementById("shopping");
+        if(x.style.display == "none") {
+                     x.style.display = "block";
+                 }
+              else {
+                     x.style.display = "none";
+      }
+  }
+
+   function tiggle() {
+       var x = document.getElementById("users");
+       if(x.style.display == "none") {
+                  x.style.display = "block";
+              }
+           else {
+                  x.style.display = "none";
+       }
+       }
+
+  function tuggle() {
+        var x = document.getElementById("games");
+        if(x.style.display == "none") {
+                       x.style.display = "block";
+                   }
+                else {
+                       x.style.display = "none";
+            }
+  }
+
+    function chat() {
+          var x = document.getElementById("chilchats");
+          if(x.style.display == "none") {
+                         x.style.display = "block";
+                     }
+                  else {
+                         x.style.display = "none";
+              }
+    }
+
+function reason() {
+              var x = document.getElementById("hahaha");
+              if(x.style.display == "none") {
+                             x.style.display = "block";
+                         }
+                      else {
+                             x.style.display = "none";
+                  }
+        }
+
+  function speaking() {
+                      var x = document.getElementById("audio");
+                      if(x.style.display == "none") {
+                                     x.style.display = "block";
+                                 }
+                              else {
+                                     x.style.display = "none";
+                          }
+                }
+
+  function multimedia() {
+                                      var x = document.getElementById("multimeria");
+                                      if(x.style.display == "none") {
+                                                     x.style.display = "block";
+                                                 }
+                                              else {
+                                                     x.style.display = "none";
+          }
+      }
+function stopped() {
+  alert("This button is not still available")
+}
+     /* HIDE N SHOW ,games,users,shopping
+
+alert("Hello User")*/
